d9baa10281ac1a9a2c13e1cd206c9845ec04f0f5
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:48 -0300

    chore: re index gatabase

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."gatabase-master".dir   = "master";
+  inputs."gatabase-master".owner = "nim-nix-pkgs";
+  inputs."gatabase-master".ref   = "master";
+  inputs."gatabase-master".repo  = "gatabase";
+  inputs."gatabase-master".type  = "github";
+  inputs."gatabase-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."gatabase-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
